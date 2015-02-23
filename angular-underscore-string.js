79642a84464d3ce0dfd293628ef7ccfd7ca52e56
Nick Lewis <nlewisinc@gmail.com>
2015-02-23 12:52:28 -0600

    + Using underscore.string's `s` global instead of a mixin.  + Renaming filter to `s` (to match underscore.string's global)

--- angular-underscore-string.js
+++ angular-underscore-string.js
@@ -7,20 +7,20 @@
  *
  * Examples:
  *
- *     {{ name | _.str: 'swapCase' }}
- *     {{ separator | _.str: 'join': ['foo', 'bar'] }}
+ *     {{ name | s: 'swapCase' }}
+ *     {{ separator | s: 'join': ['foo', 'bar'] }}
  *
- * @param {String} str String to filter
+ * @param {String} s String to filter
  * @param {String} fn Underscore.string function to call
  * @param {[params]} params Extra parameters to pass to Underscore.string
  * @return {String} Filtered string
  */
 angular.module('underscore.string', [])
-  .filter('_.str', function() {
+  .filter('s', function() {
     return function(str, fn, params) {
       str = str || '';
       params = params || [];
       params.unshift(str);
-      return fn ? _.str[fn].apply(this, params) : str;
+      return fn ? s[fn].apply(this, params) : str;
     };
   });
